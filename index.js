0ca469882e1918a3dc312794b795a4d9cd2ace4c
Benjamin Reyes <77373257+BJM43@users.noreply.github.com>
2024-01-04 23:45:14 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 80;
+
+const app = express();
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 100, // 10 mins
+    max: 5,
+})
+app.use(limiter);
+app.set('trust proxy', 1);
+
+// Set static folder
+// app.use(express.static('public'));
+
+// Routes
+app.use('/api', require('./routes'));
+
+// Enable cors
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
